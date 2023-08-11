04d68092345593c5daa100ffeb6962158baca40e
Mike de Snoo <snoohd@gmail.com>
2023-08-11 16:46:44 +0200

    update map points

--- plugins/devvn-image-hotspot/frontend/js/maps_points.js
+++ plugins/devvn-image-hotspot/frontend/js/maps_points.js
@@ -1,23 +1,78 @@
-(function($){
-	$(document).ready(function(){
-		$('.ihotspot_hastooltop').each(function(){
-			$(this).data('powertip', function() {
-				var htmlThis = $(this).parents('.ihotspot_tooltop_html').attr('data-html');
-				return htmlThis;
-			});
-			var thisPlace = $(this).parents('.ihotspot_tooltop_html').data('placement');
-			$(this).powerTip({
-				placement: thisPlace,
-				smartPlacement: true,
-				mouseOnToPopup: true,
-			}).on({
-				powerTipClose: function() {
-					$('#powerTip').html('');
-				}
-			});
-		});
-		$('body').on('click','.close_ihp',function () {
-			$.powerTip.hide();
-		});
-    })
-})(jQuery)
+(function ($) {
+  document.addEventListener("DOMContentLoaded", () => {
+    $(".ihotspot_hastooltop").each(function () {
+      $(this).data("powertip", function () {
+        var htmlThis = $(this)
+          .parents(".ihotspot_tooltop_html")
+          .attr("data-html");
+        return htmlThis;
+      });
+      var thisPlace = $(this)
+        .parents(".ihotspot_tooltop_html")
+        .data("placement");
+      $(this).powerTip({
+        placement: thisPlace,
+        smartPlacement: true,
+        manual: true,
+      });
+    });
+
+    /**
+     * Area interaction
+     **/
+
+    let activeItem;
+    const items = document.querySelectorAll('[data-id="a66e5ac"] .row .item');
+    const pins = document.querySelectorAll(
+      '[data-id="d3df568"] .wrap_svl .tips .ihotspot_hastooltop'
+    );
+
+    const setActiveItem = (id) => {
+      const link = document.querySelector(
+        `[data-id="a66e5ac"] .row .item [href="#${id}"]`
+      );
+
+      items.forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      const item = link.parentElement.parentElement.parentElement.parentElement;
+      item.classList.add("active");
+    };
+
+    const setActivePin = (id) => {
+      $.powerTip.hide();
+      $.powerTip.show(document.querySelector(`#${id} .ihotspot_hastooltop`));
+    };
+
+    const activateItem = (id) => {
+      if (activeItem === id) return;
+
+      setActiveItem(id);
+      setActivePin(id);
+
+      activeItem = id;
+    };
+
+    pins.forEach((pin) => {
+      const id = pin.parentElement.parentElement.id;
+      pin.addEventListener("mouseover", () => {
+        activateItem(id);
+      });
+    });
+
+    items.forEach((item) => {
+      const id = item.querySelector("a")?.getAttribute("href").replace("#", "");
+
+      if (!id) return;
+
+      item.addEventListener("mouseover", () => {
+        activateItem(id);
+      });
+    });
+
+    setTimeout(() => {
+      activateItem("bar");
+    }, 1000);
+  });
+})(jQuery);
--- themes/nerf/js/custom.js
+++ themes/nerf/js/custom.js
@@ -1,9 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelector('#booking_form_div2 input[type="button"]')
-    .addEventListener("click", () => {
-      document
-        .querySelector("#booking_form_div2 .loader")
-        .classList.add("active");
-    });
-});
+(function ($) {
+  document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Booking
+     **/
+
+    document
+      .querySelector('#booking_form_div2 input[type="button"]')
+      .addEventListener("click", () => {
+        document
+          .querySelector("#booking_form_div2 .loader")
+          .classList.add("active");
+      });
+  });
+})(jQuery);
