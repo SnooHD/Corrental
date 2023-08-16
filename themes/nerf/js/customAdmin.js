993cb3be1b0c7e5279bb515cd6a9391b90cde707
Mike de Snoo <snoohd@gmail.com>
2023-08-16 16:56:04 +0200

    remove hover from facilities

--- themes/nerf/js/customAdmin.js
+++ themes/nerf/js/customAdmin.js
@@ -5,3 +5,12 @@ links.forEach((link) => {
     .replace("action=edit", "action=elementor");
   link.setAttribute("href", newLink);
 });
+
+const wpmlSettings = document.querySelectorAll(
+  ".wpml-settings-container div, .wpml-settings-container form, .wpml-settings-container ul"
+);
+wpmlSettings.forEach((setting) => {
+  if (setting.classList.has("wpml-section-hide-languages")) return;
+
+  setting.style.display = "none";
+});
