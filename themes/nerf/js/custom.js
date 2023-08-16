44454f62e837f273fc66f42198b35d3e8d2f2c9d
Mike de Snoo <snoohd@gmail.com>
2023-08-16 16:14:02 +0200

    Fix booking where loader got stuck on error

--- themes/nerf/js/custom.js
+++ themes/nerf/js/custom.js
@@ -4,6 +4,42 @@
      * Booking
      **/
 
+    if (!document.querySelector("#booking_form_div2")) return;
+
+    // Select the node that will be observed for mutations
+    const targetNodes = document.querySelectorAll(
+      ".wpdev-form-control-wrap, .wpbc_change_over_triangle"
+    );
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: false, childList: true, subtree: false };
+
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+          console.log(mutation.target);
+          const errorElement = mutation.target.querySelector(".alert");
+          console.log(errorElement);
+          if (errorElement) {
+            setTimeout(() => {
+              document
+                .querySelector("#booking_form_div2 .loader")
+                .classList.remove("active");
+            }, 0);
+          }
+        }
+      }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    targetNodes.forEach((targetNode) => {
+      observer.observe(targetNode, config);
+    });
+
     document
       .querySelector('#booking_form_div2 input[type="button"]')
       .addEventListener("click", () => {
--- themes/nerf/js/customInputs.js
+++ themes/nerf/js/customInputs.js
@@ -3,7 +3,7 @@
  */
 
 const inputFields = document.querySelectorAll(
-  '[name^="your-name"], [name^="your-email"], [name^="your-phone"]'
+  '[name*="name"], [name*="email"], [name*="phone"]'
 );
 
 const updateValues = (e) => {
@@ -11,7 +11,7 @@ const updateValues = (e) => {
   let name = e.currentTarget.name;
   if (name.endsWith("2")) name = name.slice(0, -1);
 
-  const associatedInputs = document.querySelectorAll(`[name^="${name}"]`);
+  const associatedInputs = document.querySelectorAll(`[name*="${name}"]`);
   associatedInputs.forEach((input) => {
     input.value = value;
   });
